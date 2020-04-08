ace8597e311c429901d62c4696a6114c58324969
namiwa <khairul.nasri895@gmail.com>
2020-04-08 09:41:50 +0800

    feat: basic webpage

--- src/components/Uploader/Uploader.jsx
+++ src/components/Uploader/Uploader.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import ImageUploader from "react-images-upload";
+import runModel from '../Models/Models';
 
 const useStyles = makeStyles(() => ({
   root: {
-    marginTop: 50
+    marginTop: 100
   },
   appContainer: {
-    background: 'white'
+    background: '#ffffffff  '
   }
 }))
 
-export const Uploader = () => {
+export const Uploader = (props) => {
   const classes = useStyles();
+
+  const [pictures, setPictures] = useState(null);
+
+  const onDrop = picture => {
+    const src = window.URL.createObjectURL(picture[0])
+    const pic = new Image(64, 64);
+    pic.src = src;
+    setPictures([src])
+    runModel(pic).then(model => console.log(model));
+    console.log(pictures)
+  };
+
+  const handleOnClick = (e => {
+
+    e.preventDefault();
+    setPictures([]);
+    console.log(pictures)
+  })
+
+  const Preview = () => {
+    
+    return (
+      (pictures === null || pictures === []) 
+        ? (<div></div>) :
+        (<img src={pictures} alt={'thisistest'}/>) 
+    )
+  }
+
   return (
     <div className={classes.root}>
       <Container className={classes.appContainer}>
-        This is the uploader.
+        Upload any satellite image to see the classifier results!
+        <br/>
+        <br/>
+        <Button onClick={handleOnClick} variant="contained" colour="primary">Clear Image!</Button> 
+        <ImageUploader
+        {...props}
+        onChange={onDrop}
+        imgExtension={[".jpg",".png"]}
+        maxFileSize={5242880}
+        />
+        <Preview/>
       </Container>
     </div>
   )
--- src/view/Header/Header.jsx
+++ src/view/Header/Header.jsx
@@ -3,17 +3,23 @@ import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
   appBar: {
-    background: '#000000'
+    background: '#000000',
   }
 }))
 
 export const Header = () => {
   const classes = useStyles();
   return (
-    <div> 
+    <div className={classes.root}> 
       <AppBar position="fixed" className={classes.appBar}>
+        <br/>
         EG3301R Sample Web Page
+        <br/>
+        <br/>
       </AppBar>
     </div>
   )
