3ca4695eac543ecf8496f9f43dbcde691eb60abc
namiwa <khairul.nasri895@gmail.com>
2020-04-13 16:45:43 +0800

    chore: removed weights and model

--- src/components/Models/Models_rsnet.js
+++ src/components/Models/Models_rsnet.js
@@ -3,29 +3,17 @@ import * as tf from '@tensorflow/tfjs';
 
 // import all .bin and json
 import JsonModel from './first_model_web/model.json';
-import shard1 from './first_model_web/group1-shard1of23.bin';
-import shard2 from './first_model_web/group1-shard2of23.bin';
-import shard3 from './first_model_web/group1-shard3of23.bin';
-import shard4 from './first_model_web/group1-shard4of23.bin';
-import shard5 from './first_model_web/group1-shard5of23.bin';
-import shard6 from './first_model_web/group1-shard6of23.bin';
-import shard7 from './first_model_web/group1-shard7of23.bin';
-import shard8 from './first_model_web/group1-shard8of23.bin';
-import shard9 from './first_model_web/group1-shard9of23.bin';
-import shard10 from './first_model_web/group1-shard10of23.bin';
-import shard11 from './first_model_web/group1-shard11of23.bin';
-import shard12 from './first_model_web/group1-shard12of23.bin';
-import shard13 from './first_model_web/group1-shard13of23.bin';
-import shard14 from './first_model_web/group1-shard14of23.bin';
-import shard15 from './first_model_web/group1-shard15of23.bin';
-import shard16 from './first_model_web/group1-shard16of23.bin';
-import shard17 from './first_model_web/group1-shard17of23.bin';
-import shard18 from './first_model_web/group1-shard18of23.bin';
-import shard19 from './first_model_web/group1-shard19of23.bin';
-import shard20 from './first_model_web/group1-shard20of23.bin';
-import shard21 from './first_model_web/group1-shard21of23.bin';
-import shard22 from './first_model_web/group1-shard22of23.bin';
-import shard23 from './first_model_web/group1-shard23of23.bin';
+import shard1 from './first_model_web/group1-shard1of11.bin';
+import shard2 from './first_model_web/group1-shard2of11.bin';
+import shard3 from './first_model_web/group1-shard3of11.bin';
+import shard4 from './first_model_web/group1-shard4of11.bin';
+import shard5 from './first_model_web/group1-shard5of11.bin';
+import shard6 from './first_model_web/group1-shard6of11.bin';
+import shard7 from './first_model_web/group1-shard7of11.bin';
+import shard8 from './first_model_web/group1-shard8of11.bin';
+import shard9 from './first_model_web/group1-shard9of11.bin';
+import shard10 from './first_model_web/group1-shard10of11.bin';
+import shard11 from './first_model_web/group1-shard11of11.bin';
 
 
 const rawBinList = [
@@ -40,23 +28,11 @@ const rawBinList = [
   shard9,
   shard10,
   shard11,
-  shard12,
-  shard13,
-  shard14,
-  shard15,
-  shard16,
-  shard17,
-  shard18,
-  shard19,
-  shard20,
-  shard21,
-  shard22,
-  shard23,  
 ];
 
 // namelist, different for each model
 const starter = 'first_model_web/group1-shard';
-const ender = 'of23.bin';
+const ender = 'of11.bin';
 
 const names = rawBinList.map((val, ind) => {
   const j = ind + 1;
