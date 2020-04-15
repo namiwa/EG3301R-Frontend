f4a39685081128e34674599cde393d48f03ea941
namiwa <khairul.nasri895@gmail.com>
2020-04-15 21:35:40 +0800

    chore: added aditional components

---
+++ src/components/Future/FutureWork.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
+import Label from '../../assets/future/Labelling.png';
+import Load from '../../assets/future/Loading.png';
+
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginTop: 100
+  },
+}))
+
+
+export const FutureWork = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <Typography>
+        We plan to create a model which would be able to identify and predict potential areas for renewable sources of energy.
+      </Typography>
+      <Typography>
+        The model would also be able to predict the output for the 4 renewable sources we are targetting which are Hydro, Solar, Wind and Geothermal.
+      </Typography>
+      <img src={Load} alt="Loading" />
+      <img src={Label} alt="Labelling" />
+    </Card>
+  )
+}
+
+export default FutureWork;
