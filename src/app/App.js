beeb2ce2482279178c066fc188d2e8f605c0867e
namiwa <khairul.nasri895@gmail.com>
2020-09-23 00:49:26 +0800

    google maps testing

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "typeface-roboto";
 
-import Header from '../view/Header/Header';
-import Uploader from '../components/Uploader/Uploader';
-import FutureWork from '../components/Future/FutureWork';
+import Header from "../view/Header/Header";
+import Uploader from "../components/Uploader/Uploader";
+import FutureWork from "../components/Future/FutureWork";
+import Maps from "../components/Maps/Maps";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -14,12 +15,12 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route exact path={'/futurework'} component={FutureWork} /> 
-          <Route path={'/'} component={Uploader} />
+          <Route exact path={"/futurework"} component={FutureWork} />
+          <Route exact path={"/"} component={Uploader} />
+          <Route exact path={"/map"} component={Maps} />
         </Switch>
       </div>
     </Router>
-
   );
 }
 
