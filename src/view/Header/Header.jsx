d7e3fe09667a0fd8ff1b1fc3b0ad29158f10a101
namiwa <khairul.nasri895@gmail.com>
2020-09-27 20:09:34 +0800

    maps: first draft

--- src/view/Header/Header.jsx
+++ src/view/Header/Header.jsx
@@ -4,8 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Link from "@material-ui/core/Link";
 import { Link as RouterLink } from "react-router-dom";
 
@@ -27,14 +25,6 @@ export const Header = () => {
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             EG3301R ASI-304 Sample Demo Site
           </Typography>
