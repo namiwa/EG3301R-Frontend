e35ef5dcd469980a61ab5aaa2ed851837262c038
namiwa <khairul.nasri895@gmail.com>
2020-11-09 19:17:54 +0800

    fix: disabled check for self-sign cert on server

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,26 @@
+import * as https from 'http';
 import axios from 'axios';
 
+const url = 'https://35.198.192.10/';
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 export const wind_prediction_url = (lat, lng) => {
-  return `http://35.198.192.10/predictions/wind?latitude=${lat}&longtitude=${lng}`;
+  return url + `predictions/wind?latitude=${lat}&longtitude=${lng}`;
 };
 
 export const solar_prediction_url = (lat, lng) => {
-  return `http://35.198.192.10/predictions/solar?latitude=${lat}&longtitude=${lng}`;
+  return url + `predictions/solar?latitude=${lat}&longtitude=${lng}`;
 };
 
 export const geothermal_prediction_url = (lat, lng, turbine) => {
-  return `http://35.198.192.10/predictions/solar?latitude=${lat}&longtitude=${lng}&turbine=${turbine}`;
+  return (
+    url +
+    `predictions/solar?latitude=${lat}&longtitude=${lng}&turbine=${turbine}`
+  );
 };
 
-export const fetcher = (url) => axios.post(url).then((r) => r.data);
+export const fetcher = (url) =>
+  axios.post(url, { httpsAgent: agent }).then((r) => r.data);
